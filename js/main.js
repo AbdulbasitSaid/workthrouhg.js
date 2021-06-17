ce02009add902da2b7e9d287dc399dcce0282ddd
CodeSahir <aibrahim@ubtconsults.se>
2021-06-17 06:18:56 +0100

    fixed typos

--- js/main.js
+++ js/main.js
@@ -13,19 +13,19 @@ Workthrough.initialize(
             "id": 'store', 'heading': 'chech out the store', 'content': 'you will learn more by by navigating to store page', 'offset_position': 'bottom-left',
         },
         {
-            "id": 'gmail', 'heading': 'read your email', 'content': 'read your email heres', 'offset-position': 'bottom_right',
+            "id": 'gmail', 'heading': 'read your email', 'content': 'read your email heres', 'offset_position': 'bottom-right',
         },
         {
-            "id": 'apps', 'heading': 'find google apps', 'content': 'get hacking using google apps', 'offset-position': 'bottom_right',
+            "id": 'apps', 'heading': 'find google apps', 'content': 'get hacking using google apps', 'offset_position': 'bottom-right',
         },
         {
-            "id": 'sign_in', 'heading': 'Sign in here', 'content': 'to get the best experience by signing in', 'offset-position': 'bottom_right',
+            "id": 'sign_in', 'heading': 'Sign in here', 'content': 'to get the best experience by signing in', 'offset_position': 'bottom-right',
         },
         {
-            "id": 'search_container', 'heading': 'search quary here', 'content': 'put your search quary here', 'offset-position': 'bottom_left',
+            "id": 'search_container', 'heading': 'search quary here', 'content': 'put your search quary here', 'offset_position': 'bottom-left',
         },
         {
-            "id": 'advertising', 'heading': 'add heres', 'content': 'Quis ea consequat aute laborum mollit veniam mollit non incididunt proident.', 'offset-position': 'bottom_left',
+            "id": 'advertising', 'heading': 'add heres', 'content': 'Quis ea consequat aute laborum mollit veniam mollit non incididunt proident.', 'offset_position': 'bottom-left',
         },
 
 
